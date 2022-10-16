5d307f5984d7325a657fb7422a66a43b2d953a6b
mohid <mohid10587@gmail.com>
2022-10-16 09:47:07 +0600

    make env file

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 8000
 app.use(cors())
 app.use(express.json())
 
-const uri = "mongodb+srv://task_supervisor:oY8pvOWrQ6UhnvW1@cluster0.sxzjch1.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sxzjch1.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
